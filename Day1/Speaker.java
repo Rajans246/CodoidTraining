2160a4ad5ed48d76a7eaeaba4aebc1e9977d109d
Natarajan246 <natarajansenthil246@gmail.com>
2022-09-14 11:14:01 +0530

    Minor Update

--- Day1/Speaker.java
+++ Day1/Speaker.java
@@ -3,6 +3,33 @@ public class Speaker implements Volume,Play,Power{
 	public static void main(String args[]){
 	
 	}
+    public void play(){
+        System.out.println("play");
+        
+    }
+    public void pause(){
+        System.out.println("pause");
+    }
 	
+	public void powerOn(){
+	    System.out.println("on");
+	}
+    
+    public void powerOff(){
+	    System.out.println("off");
+	}
 
+    public void volumeUp(){
+        System.out.println("up");
+    }
+    
+    public void volumeDown(){
+        System.out.println("down");
+        
+    }
+    
+    public void mute(){
+        
+        System.out.println("mute");
+    }
 }
