ba9d636304418ea9e052a651e0aa07d2dda4e9f4
Natarajan246 <natarajansenthil246@gmail.com>
2022-09-23 12:34:06 +0530

    Hacker Rank Problems.

---
+++ HackerRank/StaticBlockProgram.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class StaticBlockProgram {
+
+// Write your code here
+static{
+    
+    Scanner sc = new Scanner(System.in);
+    
+    int B = sc.nextInt();
+    int H = sc.nextInt();
+    
+    if(B < 0 || H < 0 ){
+        flag = True;
+    }
+}
+
+public static void main(String[] args){
