59b56f144af4ce1278de4dc07cf8d6416dda415e
Natarajan246 <natarajansenthil246@gmail.com>
2022-09-26 19:59:29 +0530

    Get Currency type program in hacker rank. Modified DayFinder program

--- Day6/DayFinder.java
+++ Day6/DayFinder.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class DayFinder {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
